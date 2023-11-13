ef25ac604df3edc6cee9ee9d2322369761db49fa
One-1002 <145385281+One-1002@users.noreply.github.com>
2023-11-12 18:26:08 -0600

    Agregar archivos de proyecto.

---
+++ ExamenPrectico3/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace ExamenPrectico3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(94, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(94, 131);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(116, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Usuario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(116, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Fecha";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(68, 285);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 44);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Encender";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(210, 285);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 44);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Encender";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(66, 335);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(114, 37);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Apagar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(210, 335);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(105, 37);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Apagar";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(103, 233);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "LED";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(232, 233);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "MOTOR";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(394, 108);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(177, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Temperatura en Celsius";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(585, 108);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(203, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Temperatura en Fahrenheit";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(458, 137);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 20);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(525, 209);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(101, 20);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "RAWVALUE";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(662, 137);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(51, 20);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "label9";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(549, 245);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(60, 20);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "label10";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(529, 335);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(119, 55);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "Guardar";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ ExamenPrectico3/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.IO;
+using System.Text.RegularExpressions;
+using MySql.Data.MySqlClient;
+using System.Data.SqlClient;
+
+
+namespace ExamenPrectico3
+{
+    public partial class Form1 : Form
+    {
+        //Datos de conexion a MySQL (XAMPP)
+        string conexionSQL = "Server=localhost;Port=3306;Database=examenpractico;Uid=root;Pwd=;";
+        //Metodo de para insertar registros
+        public SerialPort ArduinoPort
+        {
+            get;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            //crear Serial Port
+            ArduinoPort = new System.IO.Ports.SerialPort();
+            ArduinoPort.PortName = "COM13";
+            ArduinoPort.BaudRate = 9600;
+            ArduinoPort.DataBits = 8;
+            ArduinoPort.ReadTimeout = 500;
+            ArduinoPort.WriteTimeout = 500;
+            ArduinoPort.Open();
+            // vincular eventos
+            this.FormClosing += CerrandoForm1;
+            this.button1.Click += button1_Click;
+            this.button3.Click += button3_Click;
+            this.button2.Click += button2_Click;
+            this.button4.Click += button4_Click;
+        }
+        private void InsertarRegistros(string nombre, string Fecha, float TempC, float TempF, float RAWVAL)
+        {
+            using (MySqlConnection conection = new MySqlConnection(conexionSQL))
+            {
+                conection.Open();
+                string insertQuery = "INSERT INTO registros (Nombre, Fecha, TempC, TempF, RAWVAL)" +
+                    "VALUES (@Nombre, @Fecha, @TempC, @TempF, @RAWVAL)";
+
+                using (MySqlCommand command = new MySqlCommand(insertQuery, conection))
+                {
+                    command.Parameters.AddWithValue("@Nombre", nombre);
+                    command.Parameters.AddWithValue("@Fecha", Fecha );
+                    command.Parameters.AddWithValue("@TempC", TempC);
+                    command.Parameters.AddWithValue("@TempF", TempF);
+                    command.Parameters.AddWithValue("@RAWVAL", RAWVAL);
+                    command.ExecuteNonQuery();
+                }
+                conection.Close();
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ArduinoPort.Write("b");
+        }
+        private void CerrandoForm1(object sender, EventArgs e)
+        {
+            //cerrar puerto
+            if (ArduinoPort.IsOpen) ArduinoPort.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ArduinoPort.Write("a");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!ArduinoPort.IsOpen)
+                ArduinoPort.Open();
+            ArduinoPort.Write("b");
+            button2.Enabled = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ArduinoPort.Write("a");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //obtener los datos de los textbox
+            string nombre = textBox1.Text;
+            string fecha = textBox2.Text;
+
+            //Guardar los datos en un archivo de texto
+            string rutaArchivo = "C:\\Users\\danpa\\OneDrive\\Escritorio\\Semestre 3\\Ruta\\Archivooneida.txt";
+            bool archivoExiste = File.Exists(rutaArchivo);
+            if (archivoExiste == false)
+            {
+               // File.WriteAllText(rutaArchivo, datos);
+            }
+            else
+            {
+                //Verificar si el archivo ya existe
+                using (StreamWriter writer = new StreamWriter(rutaArchivo, true))
+                {
+                    if (archivoExiste)
+                    {
+                        //si el archivo existe, añadir un separador antes del nuevo registro
+                        writer.WriteLine();
+                        //Programacion de funcionalidad de insert SQL
+                        InsertarRegistros(nombre, fecha);
+                        MessageBox.Show("Datos ingresados correctamente");
+                    }
+                    else
+                    {
+                        writer.Write(datos);
+                        //Programacion de funcionalidad de insert SQL
+                        InsertarRegistros(nombres, apellido, int.Parse(edad), decimal.Parse(estatura), telefono, genero);
+                        MessageBox.Show("Datos ingresados correctamente");
+                    }
+                }
+            }
+            //Mostrar un mensaje con los datos capturados
+            MessageBox.Show("Datos guardados con éxito:\n\n" + datos, "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+            {
+                MessageBox.Show("Por favor, ingrese datos validos en los campos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
+        
+    
+
